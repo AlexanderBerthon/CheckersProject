6ea6da2d2260fc74470299f6af01cf280e9b4442
Alex Berthon <Alexjberthon@gmail.com>
2022-02-02 15:23:50 -0800

    TabIndex Refactor I was under the assumption that I needed to start the tab index at 1 rather than 0 in order to distinguish rows from eachother and ensure proper gameplay. But it turns out I can distinguish columns just as easily by starting at 0, and I made a miscalculation when using the % opperator. The mismatch of layout wanting to start at 0 and the UI programmed to start at 1 was causing a lot of inconsistencies in the behavior of the code. So I went ahead and modified all of the code to work off of the 0 index start baseline. This was a major setback but will improve the code greatly, making it more readable, understandable, and consistent.

--- Form1.cs
+++ Form1.cs
@@ -9,12 +9,44 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
- * REMEMBER OFF BY 1 ERRORS: Anytime you reference flowLayout1, layout will be -1. button = 10 layout thinks it's 9 
- * but they will modify the SAME button. just different reference. keep that in mind!
-	raw capture values - AI (top to bottom)
-	//7, 9 - adjacent(left, right)
-	//14, 18 - target(left, right)
- */
+TabIndex mapping
+00 01 02 03 04 05 06 07
+08 09 10 11 12 13 14 15
+16 17 18 19 20 21 22 23
+24 25 26 27 28 29 30 31 
+32 33 34 35 36 37 38 39
+40 41 42 43 44 45 46 47
+48 49 50 51 52 53 54 55
+56 57 58 59 60 61 62 63
+
+i % 8 mapping
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7
+
+col1 ~ i%8 = 0
+col2 ~ i%8 = 1
+col7 ~ i%8 = 6
+col8 ~ i%8 = 7
+
+Top-down (AI)
+Left-Move	 index +7
+Right-Move	 index +9
+Left-Cap	 index +7, index +14
+Right-Cap	 index +9, index +18 
+
+Bot-up (p1)
+Left-Move	 index -9
+Right-Move	 index -7
+Left-Cap	 index -9, index -18
+Right-Cap	 index -7, index -14
+
+*/
 
 namespace CheckersProject {
     public partial class Form1 : Form {
@@ -35,6 +67,7 @@ namespace CheckersProject {
 			flowLayoutPanel1.Controls.CopyTo(temp, 0);
 		}
 		
+
 		private void button_Click(object sender, EventArgs e) {
 			if (playerTurn) {
 				Button button = (Button)sender;
@@ -54,7 +87,7 @@ namespace CheckersProject {
 				else {
 					Boolean validMove = false;
 					//check move for left column exception
-					if (selected.TabIndex % 8 == 1) {
+					if (selected.TabIndex % 8 == 0) {
 						//normal move check
 						if (selected.TabIndex == button.TabIndex + 7 && button.BackgroundImage == null) {
 							label1.Text = "left column, good move right";
@@ -62,10 +95,10 @@ namespace CheckersProject {
 						}
 						//capture move check
 						else if (selected.TabIndex == (button.TabIndex + 14) && button.BackgroundImage == null) {
-							if (flowLayoutPanel1.Controls[selected.TabIndex - 8].Tag == "RCoin") {
+							if (flowLayoutPanel1.Controls[selected.TabIndex - 7].Tag == "RCoin") {
 								label1.Text = "left column, good capture right";
-								flowLayoutPanel1.Controls[selected.TabIndex - 8].BackgroundImage = null;
-								flowLayoutPanel1.Controls[selected.TabIndex - 8].Tag = null;
+								flowLayoutPanel1.Controls[selected.TabIndex - 7].BackgroundImage = null;
+								flowLayoutPanel1.Controls[selected.TabIndex - 7].Tag = null;
 								//player1Points++
 								validMove = true;
 							}
@@ -75,7 +108,7 @@ namespace CheckersProject {
 						}
 					}
 					//check move for right column exception
-					else if (selected.TabIndex % 8 == 0) {
+					else if (selected.TabIndex % 8 == 7) {
 						//normal move check
 						if (selected.TabIndex == button.TabIndex + 9 && button.BackgroundImage == null) {
 							label1.Text = "right column, good move left";
@@ -83,10 +116,10 @@ namespace CheckersProject {
 						}
 						//capture move check
 						else if (selected.TabIndex == (button.TabIndex + 18) && button.BackgroundImage == null) {
-							if (flowLayoutPanel1.Controls[selected.TabIndex - 10].Tag == "RCoin") {
+							if (flowLayoutPanel1.Controls[selected.TabIndex - 9].Tag == "RCoin") {
 								label1.Text = "right column, good capture left";
-								flowLayoutPanel1.Controls[selected.TabIndex - 10].BackgroundImage = null;
-								flowLayoutPanel1.Controls[selected.TabIndex - 10].Tag = null;
+								flowLayoutPanel1.Controls[selected.TabIndex - 9].BackgroundImage = null;
+								flowLayoutPanel1.Controls[selected.TabIndex - 9].Tag = null;
 								//player1Points++
 								validMove = true;
 							}
@@ -106,20 +139,20 @@ namespace CheckersProject {
 					}
 					//check capture move up-left
 					else if (selected.TabIndex == (button.TabIndex + 18) && button.BackgroundImage == null) {//
-						if (flowLayoutPanel1.Controls[selected.TabIndex - 10].Tag == "RCoin") {
+						if (flowLayoutPanel1.Controls[selected.TabIndex - 9].Tag == "RCoin") {
 							label1.Text = "good capture left";
-							flowLayoutPanel1.Controls[selected.TabIndex - 10].BackgroundImage = null;
-							flowLayoutPanel1.Controls[selected.TabIndex - 10].Tag = null;
+							flowLayoutPanel1.Controls[selected.TabIndex - 9].BackgroundImage = null;
+							flowLayoutPanel1.Controls[selected.TabIndex - 9].Tag = null;
 							//player1Points++
 							validMove = true;
 						}
 					}
 					//check capture move up-right
 					else if (selected.TabIndex == (button.TabIndex + 14) && button.BackgroundImage == null) {
-						if (flowLayoutPanel1.Controls[selected.TabIndex - 8].Tag == "RCoin") {
+						if (flowLayoutPanel1.Controls[selected.TabIndex - 7].Tag == "RCoin") {
 							label1.Text = "good capture right";
-							flowLayoutPanel1.Controls[selected.TabIndex - 8].BackgroundImage = null;
-							flowLayoutPanel1.Controls[selected.TabIndex - 8].Tag = null;
+							flowLayoutPanel1.Controls[selected.TabIndex - 7].BackgroundImage = null;
+							flowLayoutPanel1.Controls[selected.TabIndex - 7].Tag = null;
 							//player1Points++
 							validMove = true;
 						}
@@ -135,8 +168,7 @@ namespace CheckersProject {
 						selected.Tag = null;
 						selected = null;
 						playerTurn = false;
-						AITurn();
-						//end turn
+						AITurn(); //end player turn (might need and if() check for multi-moves above this)
 					}
 					else {
 						label1.Text = "Invalid Move";
@@ -149,7 +181,7 @@ namespace CheckersProject {
 			this.Refresh(); //must repaint manually, since this is called within a single click action
 			System.Threading.Thread.Sleep(1000);
 
-			flowLayoutPanel1.Controls.CopyTo(temp, 0); //does this override previous entries? or cause an exception??
+			flowLayoutPanel1.Controls.CopyTo(temp, 0);
 			foreach (Button button in temp) {
 				if (button.Tag == "RCoin") {
 					AIPieces.Add(button);
@@ -160,58 +192,58 @@ namespace CheckersProject {
 			try {
 				foreach (Button button in AIPieces) {
 					//if piece is in column 1 or 2, can only attempt right-sided capture.
-					if ((button.TabIndex % 8 == 1 ||button.TabIndex % 8 == 2)
-					&& flowLayoutPanel1.Controls[button.TabIndex + 17].BackgroundImage == null
-					&& flowLayoutPanel1.Controls[button.TabIndex + 8].Tag == "BCoin") {
+					if ((button.TabIndex % 8 == 0 ||button.TabIndex % 8 == 1)
+					&& flowLayoutPanel1.Controls[button.TabIndex + 18].BackgroundImage == null
+					&& flowLayoutPanel1.Controls[button.TabIndex + 9].Tag == "BCoin") {
 						label1.Text = "good-capture-right (exception)";
-						flowLayoutPanel1.Controls[button.TabIndex + 8].BackgroundImage = null;
-						flowLayoutPanel1.Controls[button.TabIndex + 8].Tag = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 9].BackgroundImage = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 9].Tag = null;
 						//AIPoints++
-						flowLayoutPanel1.Controls[button.TabIndex + 17].BackgroundImage = button.BackgroundImage;
-						flowLayoutPanel1.Controls[button.TabIndex + 17].Tag = button.Tag;
+						flowLayoutPanel1.Controls[button.TabIndex + 18].BackgroundImage = button.BackgroundImage;
+						flowLayoutPanel1.Controls[button.TabIndex + 18].Tag = button.Tag;
 						button.BackgroundImage = null;
 						button.Tag = null;
 						playerTurn = true;
 						break;
 					}
 					//if piece is in column 7 or 8 can only attempt left-sided capture.
-					else if ((button.TabIndex % 8 == 7 || button.TabIndex % 8 == 0)
-					&& flowLayoutPanel1.Controls[button.TabIndex + 13].BackgroundImage == null
-					&& flowLayoutPanel1.Controls[button.TabIndex + 6].Tag == "BCoin") {
+					else if ((button.TabIndex % 8 == 6 || button.TabIndex % 8 == 7)
+					&& flowLayoutPanel1.Controls[button.TabIndex + 14].BackgroundImage == null
+					&& flowLayoutPanel1.Controls[button.TabIndex + 7].Tag == "BCoin") {
 						label1.Text = "good-capture-left (exception)";
-						flowLayoutPanel1.Controls[button.TabIndex + 6].BackgroundImage = null;
-						flowLayoutPanel1.Controls[button.TabIndex + 6].Tag = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 7].BackgroundImage = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 7].Tag = null;
 						//AIPoints++
-						flowLayoutPanel1.Controls[button.TabIndex + 13].BackgroundImage = button.BackgroundImage;
-						flowLayoutPanel1.Controls[button.TabIndex + 13].Tag = button.Tag;
+						flowLayoutPanel1.Controls[button.TabIndex + 14].BackgroundImage = button.BackgroundImage;
+						flowLayoutPanel1.Controls[button.TabIndex + 14].Tag = button.Tag;
 						button.BackgroundImage = null;
 						button.Tag = null;
 						playerTurn = true;
 						break;
 					}
 					//check if piece can make a left capture
-					else if (flowLayoutPanel1.Controls[button.TabIndex + 13].BackgroundImage == null
-					&& flowLayoutPanel1.Controls[button.TabIndex + 6].Tag == "BCoin") {
+					else if (flowLayoutPanel1.Controls[button.TabIndex + 14].BackgroundImage == null
+					&& flowLayoutPanel1.Controls[button.TabIndex + 7].Tag == "BCoin") {
 						label1.Text = "good-capture-left (normal)";
-						flowLayoutPanel1.Controls[button.TabIndex + 6].BackgroundImage = null;
-						flowLayoutPanel1.Controls[button.TabIndex + 6].Tag = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 7].BackgroundImage = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 7].Tag = null;
 						//AIPoints++
-						flowLayoutPanel1.Controls[button.TabIndex + 13].BackgroundImage = button.BackgroundImage;
-						flowLayoutPanel1.Controls[button.TabIndex + 13].Tag = button.Tag;
+						flowLayoutPanel1.Controls[button.TabIndex + 14].BackgroundImage = button.BackgroundImage;
+						flowLayoutPanel1.Controls[button.TabIndex + 14].Tag = button.Tag;
 						button.BackgroundImage = null;
 						button.Tag = null;
 						playerTurn = true;
 						break;
 					}
 					//check if piece can make a right capture
-					else if (flowLayoutPanel1.Controls[button.TabIndex + 17].BackgroundImage == null
-					&& flowLayoutPanel1.Controls[button.TabIndex + 8].Tag == "BCoin") {
+					else if (flowLayoutPanel1.Controls[button.TabIndex + 18].BackgroundImage == null
+					&& flowLayoutPanel1.Controls[button.TabIndex + 9].Tag == "BCoin") {
 						label1.Text = "good-capture-right (normal)";
-						flowLayoutPanel1.Controls[button.TabIndex + 8].BackgroundImage = null;
-						flowLayoutPanel1.Controls[button.TabIndex + 8].Tag = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 9].BackgroundImage = null;
+						flowLayoutPanel1.Controls[button.TabIndex + 9].Tag = null;
 						//AIPoints++
-						flowLayoutPanel1.Controls[button.TabIndex + 17].BackgroundImage = button.BackgroundImage;
-						flowLayoutPanel1.Controls[button.TabIndex + 17].Tag = button.Tag;
+						flowLayoutPanel1.Controls[button.TabIndex + 18].BackgroundImage = button.BackgroundImage;
+						flowLayoutPanel1.Controls[button.TabIndex + 18].Tag = button.Tag;
 						button.BackgroundImage = null;
 						button.Tag = null;
 						playerTurn = true;
